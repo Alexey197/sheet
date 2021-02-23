c1cbc1b0b9016933d4ea49bd30909a5924c7219b
Alexey197 <margadonnn3@gmail.com>
2021-02-23 09:02:48 +0300

    modules

---
+++ src/templates.js
@@ -0,0 +1,11 @@
+export function row(block) {
+  const html = block.value.map(item => `<div class="cell">${item}</div>`).join('')
+  return `
+    <div class="row">
+      ${html}
+    </div>
+    <div class="tip">
+      ${block.text}
+    </div>
+  `
+}
