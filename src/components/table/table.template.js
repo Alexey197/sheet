f96dca20f683326dbfade8f914c9e28013a7dd0f
Alexey197 <margadonnn3@gmail.com>
2021-06-05 10:13:27 +0300

    massive map

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -13,18 +13,26 @@ function createRow(content) {
 }
 
 export function createTable() {
-  const colsCount = rowsArr[0].length
-  const rowsCount = rowsArr.length
+  // const colsCount = rowsArr[0].length
+  // const rowsCount = rowsArr.length
   const rows = []
-  
-  for (let i = 0; i < rowsCount; i++) {
+  rowsArr.map(el => {
     const row = []
-    for (let j = 0; j < rowsArr[i].length; j ++) {
-      const cell = (createCell(rowsArr[i][j]))
+    el.map(el => {
+      const cell = createCell(el)
       row.push(cell)
-    }
+    })
     rows.push(createRow(row.join('')))
-  }
+  })
+  //
+  // for (let i = 0; i < rowsCount; i++) {
+  //   const row = []
+  //   for (let j = 0; j < rowsArr[i].length; j ++) {
+  //     const cell = (createCell(rowsArr[i][j]))
+  //     row.push(cell)
+  //   }
+  //   rows.push(createRow(row.join('')))
+  // }
   
   return rows.join('')
 }
